3675059b647e2af0e0f4285c38ee553d2d1ce697
cielavenir <cielartisan@gmail.com>
2021-08-27 16:34:42 +0900

    try package_data approach (still failing)

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='package_data_symlink',
     version='0.0.0.1',
     packages=['package_data_symlink'],
-    include_package_data=True,
+    # include_package_data=True,
+    package_data={'package_data_symlink':['data1/hello.txt','data2']},
     license='0BSD',
 )
