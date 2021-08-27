7fd8978c46e6970110eabff011102e483f8e2474
cielavenir <cielartisan@gmail.com>
2021-08-27 16:24:10 +0900

    add setup.py (yup I know this does not work (yet))

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='package_data_symlink',
+    version='0.0.0.1',
+    packages=['package_data_symlink'],
+    include_package_data=True,
+    license='0BSD',
+)
